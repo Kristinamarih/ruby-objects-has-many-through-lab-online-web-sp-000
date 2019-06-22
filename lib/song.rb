f6545403ac570fee0d59691bbbcd9f42af691d08
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-22 16:34:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@ class Song
   attr_accessor :name, :artist, :genre 
  @@all = []
  
- def initialize(name)
+ def initialize(name, artist, genre)
    @name = name 
    @artist = artist 
    @genre = genre 
