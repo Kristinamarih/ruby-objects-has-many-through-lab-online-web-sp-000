c879858cad60109ae70e583a48116e423ad5405c
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-06-22 16:31:30 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,19 @@
+class Genre 
+  
+  attr_accessor :name 
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def songs 
+    Songs.all.select do |song|
+      song.self 
+  
+end
